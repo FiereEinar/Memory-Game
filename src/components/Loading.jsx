0cf2155cef5d0fe0263ad28d217e2be520498c48
Nick Melloria <nickxylanmelloria@gmail.com>
2024-02-28 14:52:51 +0800

    initial deployment

--- src/components/Loading.jsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div className='text-white w-full h-full flex justify-center
-    pt-20'>
-      <p className='text-4xl'>Loading Pokemons...</p>
-    </div>
-  )
-}
